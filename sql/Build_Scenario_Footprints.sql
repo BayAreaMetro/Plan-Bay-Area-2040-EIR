00f32fda2ebc83748e8aafbb4612288401c4abe3
Tom Buckley <buckley.tom@gmail.com>
2017-01-10 15:41:50 -0800

    alias tablenames
    this will make the views more readable as i add calculations for density back in

--- sql/Build_Scenario_Footprints.sql
+++ sql/Build_Scenario_Footprints.sql
@@ -9,76 +9,77 @@
 --select count(objectid) From UrbanSim.UrbanSim_Parcels
 --Total Parcels (1956208)
 Create View Analysis.run10_parcel_output_spatial as
-SELECT  Analysis.p09_01_2015_parcel_shareable.COUNTY_ID as county_id, 
-		Analysis.run10_parcel_output.parcel_id as parcel_id, 
-		cast(Analysis.run10_parcel_output.year_built as integer) as year_built,
-		cast(Analysis.run10_parcel_output.job_spaces as integer) as job_spaces,
-		cast(Analysis.run10_parcel_output.total_residential_units as integer) as total_residential_units,
-		Analysis.p09_01_2015_parcel_shareable.Shape as Shape
-FROM Analysis.run10_parcel_output
-LEFT JOIN Analysis.p09_01_2015_parcel_shareable
-ON Analysis.p09_01_2015_parcel_shareable.parcel_id = Analysis.run10_parcel_output.parcel_id
-WHERE cast(Analysis.run10_parcel_output.year_built AS integer)>2015
+SELECT  p.COUNTY_ID as county_id, 
+		r.parcel_id as parcel_id, 
+		cast(r.year_built as integer) as year_built,
+		cast(r.job_spaces as integer) as job_spaces,
+		cast(r.total_residential_units as integer) as total_residential_units,
+		Round(p.shape.STArea()*0.000247105381,2) as Acres,
+		Cast((2.69*r.total_residential_units)/(p.shape.STArea()*0.000247105381) as numeric(18,2)) as People_Per_Acre,
+		Cast((r.job_spaces/(p.shape.STArea()*0.000247105381)) as numeric(18,2)) as Jobs_Per_Acre,
+		p.Shape as Shape
+FROM Analysis.run10_parcel_output as r
+LEFT JOIN Analysis.p09_01_2015_parcel_shareable as p
+ON p.parcel_id = r.parcel_id
+WHERE cast(r.year_built AS integer)>2015
 
 GO
 
 CREATE VIEW Analysis.run17_parcel_output_spatial AS
-SELECT Analysis.p09_01_2015_parcel_shareable.COUNTY_ID as county_id, 
-Analysis.run17_parcel_output.parcel_id as parcel_id, 
-cast(Analysis.run17_parcel_output.year_built as integer) asyear_built,
-cast(Analysis.run17_parcel_output.job_spaces as integer) asjob_spaces,
-cast(Analysis.run17_parcel_output.total_residential_units as integer) astotal_residential_units,
-Analysis.p09_01_2015_parcel_shareable.Shape as Shape
-FROM Analysis.run17_parcel_output
+SELECT  Analysis.p09_01_2015_parcel_shareable.COUNTY_ID as county_id, 
+		r.parcel_id as parcel_id, 
+		cast(r.year_built as integer) asyear_built,
+		cast(r.job_spaces as integer) asjob_spaces,
+		cast(r.total_residential_units as integer) astotal_residential_units,
+		p.Shape as Shape
+FROM Analysis.run17_parcel_output as r
 left join Analysis.p09_01_2015_parcel_shareable
-on Analysis.p09_01_2015_parcel_shareable.parcel_id = Analysis.run17_parcel_output.parcel_id
+on p.parcel_id = Analysis.run17_parcel_output.parcel_id
 WHERE cast(Analysis.run17_parcel_output.year_built as integer)>2015
 
 GO
 
 CREATE VIEW Analysis.run11_parcel_output_spatial AS
-SELECT Analysis.p09_01_2015_parcel_shareable.COUNTY_ID as county_id, 
-Analysis.run11_parcel_output.parcel_id as parcel_id, 
-cast(Analysis.run11_parcel_output.year_built as integer) as year_built,
-cast(Analysis.run11_parcel_output.job_spaces as integer) as job_spaces,
-cast(Analysis.run11_parcel_output.total_residential_units as integer) as total_residential_units,
-Analysis.p09_01_2015_parcel_shareable.Shape as Shape
-FROM Analysis.run11_parcel_output
+SELECT  p.COUNTY_ID as county_id, 
+		r.parcel_id as parcel_id, 
+		cast(r.year_built as integer) as year_built,
+		cast(r.job_spaces as integer) as job_spaces,
+		cast(r.total_residential_units as integer) as total_residential_units,
+		p.Shape as Shape
+FROM Analysis.run11_parcel_output as r
 left join Analysis.p09_01_2015_parcel_shareable
-on Analysis.p09_01_2015_parcel_shareable.parcel_id = Analysis.run11_parcel_output.parcel_id
-WHERE cast(Analysis.run11_parcel_output.year_built as integer)>2015;
+on p.parcel_id = r.parcel_id
+WHERE cast(r.year_built as integer)>2015;
 
 GO 
 
 CREATE VIEW Analysis.run12_parcel_output_spatial AS
-SELECT Analysis.p09_01_2015_parcel_shareable.COUNTY_ID as county_id, 
-Analysis.run12_parcel_output.parcel_id as parcel_id, 
-cast(Analysis.run12_parcel_output.year_built as integer) asyear_built,
-cast(Analysis.run12_parcel_output.job_spaces as integer) asjob_spaces,
-cast(Analysis.run12_parcel_output.total_residential_units as integer) astotal_residential_units,
-Analysis.p09_01_2015_parcel_shareable.Shape as Shape
-FROM Analysis.run12_parcel_output
+SELECT  p.COUNTY_ID as county_id, 
+		r.parcel_id as parcel_id, 
+		cast(r.year_built as integer) asyear_built,
+		cast(r.job_spaces as integer) asjob_spaces,
+		cast(r.total_residential_units as integer) astotal_residential_units,
+		p.Shape as Shape
+FROM Analysis.run12_parcel_output as r
 left join Analysis.p09_01_2015_parcel_shareable
-on Analysis.p09_01_2015_parcel_shareable.parcel_id = Analysis.run12_parcel_output.parcel_id
-WHERE cast(Analysis.run12_parcel_output.year_built as integer)>2015;
+on p.parcel_id = r.parcel_id
+WHERE cast(r.year_built as integer)>2015;
 
 GO 
 
 CREATE VIEW Analysis.run7224_parcel_output_spatial AS
-SELECT Analysis.p09_01_2015_parcel_shareable.COUNTY_ID as county_id, 
-Analysis.run7224_parcel_output.parcel_id as parcel_id, 
-cast(Analysis.run7224_parcel_output.year_built as integer) asyear_built,
-cast(Analysis.run7224_parcel_output.job_spaces as integer) asjob_spaces,
-cast(Analysis.run7224_parcel_output.total_residential_units as integer) astotal_residential_units,
-Analysis.p09_01_2015_parcel_shareable.Shape as Shape
-FROM Analysis.run7224_parcel_output
+SELECT  p.COUNTY_ID as county_id, 
+		r.parcel_id as parcel_id, 
+		cast(r.year_built as integer) asyear_built,
+		cast(r.job_spaces as integer) asjob_spaces,
+		cast(r.total_residential_units as integer) astotal_residential_units,
+		p.Shape as Shape
+FROM Analysis.run7224_parcel_output as r
 left join Analysis.p09_01_2015_parcel_shareable
-on Analysis.p09_01_2015_parcel_shareable.parcel_id = Analysis.run7224_parcel_output.parcel_id
-WHERE cast(Analysis.run7224_parcel_output.year_built as integer)>2015;
+on p.parcel_id = r.parcel_id
+WHERE cast(r.year_built as integer)>2015;
 
 /*Cast(2.69*alt_4_Diff.total_residential_units as numeric(18,0)) as Estimated_Population, 
-Cast(alt_4_Diff.total_residential_units as numeric(18,0)) as total_residential_units, 
-alt_4_Diff.total_job_spaces, 
 Round(p.shape.STArea()*0.000247105381,2) as Acres,
 Cast((2.69*alt_4_Diff.total_residential_units)/(p.shape.STArea()*0.000247105381) as numeric(18,2)) as People_Per_Acre,
 Cast((alt_4_Diff.total_job_spaces/(p.shape.STArea()*0.000247105381)) as numeric(18,2)) as Jobs_Per_Acre,
