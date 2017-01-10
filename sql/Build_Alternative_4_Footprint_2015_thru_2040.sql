2dd2357844e3bd96ded92a9230b2f7c284d358a8
Tom Buckley <buckley.tom@gmail.com>
2017-01-10 14:16:30 -0800

    simplify query-comment out calculations
    i'm going to move the calculations of people per acre and area to another view that points to this one, just to keep the block for each query a bit smaller. hopefully this will make the multiple queires more readable.

--- sql/Build_Alternative_4_Footprint_2015_thru_2040.sql
+++ sql/Build_Alternative_4_Footprint_2015_thru_2040.sql
@@ -8,62 +8,25 @@
 --Because the alt output does not contain ppl per parcel, an average hhsize of 2.69 was used to estimate population for each parcel based upon nthe number of res. units on that parcel.
 --select count(objectid) From UrbanSim.UrbanSim_Parcels
 --Total Parcels (1956208)
+Create View Analysis.run10_parcel_output_spatial as
+SELECT  Analysis.p09_01_2015_parcel_shareable.COUNTY_ID as county_id, 
+		Analysis.run10_parcel_output.parcel_id as parcel_id, 
+		cast(Analysis.run10_parcel_output.year_built as integer) as year_built,
+		cast(Analysis.run10_parcel_output.job_spaces as integer) as job_spaces,
+		cast(Analysis.run10_parcel_output.total_residential_units as integer) as total_residential_units,
+		Analysis.p09_01_2015_parcel_shareable.Shape as Shape
+FROM Analysis.run10_parcel_output
+LEFT JOIN Analysis.p09_01_2015_parcel_shareable
+ON Analysis.p09_01_2015_parcel_shareable.parcel_id = Analysis.run10_parcel_output.parcel_id
+WHERE cast(Analysis.run10_parcel_output.year_built AS integer)>2015
 
-USE [gis]
-Go
-Drop view UrbanSim.Alt_4_GrowthFootprint
-Go
-create view UrbanSim.Alt_4_GrowthFootprint as
-SELECT
---Using Distinct due to duplicate parcels in the Parcel Geometry Table (11271 Parcels selected)
-Distinct
-Top 20000
-p.OBJECTID, 
-p.COUNTY_ID, 
-alt_4_bldg.parcel_id, 
-alt_4_bldg.year_built,
-Cast(2.69*alt_4_Diff.total_residential_units as numeric(18,0)) as Estimated_Population, 
+/*Cast(2.69*alt_4_Diff.total_residential_units as numeric(18,0)) as Estimated_Population, 
 Cast(alt_4_Diff.total_residential_units as numeric(18,0)) as total_residential_units, 
 alt_4_Diff.total_job_spaces, 
 Round(p.shape.STArea()*0.000247105381,2) as Acres,
 Cast((2.69*alt_4_Diff.total_residential_units)/(p.shape.STArea()*0.000247105381) as numeric(18,2)) as People_Per_Acre,
 Cast((alt_4_Diff.total_job_spaces/(p.shape.STArea()*0.000247105381)) as numeric(18,2)) as Jobs_Per_Acre,
---Convert shape to string to remove duplicates from parcel geometry table.
-p.Shape.ToString() as Features
-FROM            
-UrbanSim.RUN7224_PARCEL_DATA_DIFF AS alt_4_Diff INNER JOIN
-UrbanSim.UrbanSim_Parcels AS p ON alt_4_Diff.parcel_id = p.PARCEL_ID RIGHT OUTER JOIN
-UrbanSim.RUN7224_BUILDING_DATA_2040 AS alt_4_bldg ON p.PARCEL_ID = alt_4_bldg.parcel_id
-WHERE        
-(alt_4_bldg.year_built >= 2016) AND (alt_4_Diff.total_residential_units > 0) OR
-(alt_4_Diff.total_job_spaces > 0)
-Order By County_ID
-Go
-Drop View UrbanSim.Alt_4_GrowthFootprint_Features 
---Filter the growth footprint based upon the EIR Development Threshold (8 ppl/ac or 10 jobs/ac).
---Convert WKT to Geometry
---geometry::STGeomFromText(Features,26910).MakeValid() as Shape
-Go
-create view UrbanSim.Alt_4_GrowthFootprint_Features as
-SELECT        
-OBJECTID, 
-COUNTY_ID, 
-parcel_id, 
-year_built, 
-Estimated_Population, 
-total_residential_units, 
-total_job_spaces, 
-Acres, 
-People_Per_Acre, 
-Jobs_Per_Acre, 
---Features
-geometry::STGeomFromText(Features,26910).MakeValid() as Shape
---Drop Table UrbanSim.Alt_4_GFP_FC
---into UrbanSim.Alt_4_GFP_FC
-FROM            UrbanSim.Alt_4_GrowthFootprint
 WHERE        (People_Per_Acre >= 8) OR
                          (Jobs_Per_Acre >= 10)
-Go
-select * From UrbanSim.Alt_4_GrowthFootprint_Features
 
 
