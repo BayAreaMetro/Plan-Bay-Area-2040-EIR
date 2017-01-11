0a1364a7140e1c7b8247dc2e3b8b5d549a2a5349
Tom Buckley <buckley.tom@gmail.com>
2017-01-10 18:50:33 -0800

    compare output footprints for alt 4
    these seem pretty different. i think that may be due to the fact that the alt 4 query was pulling from the buildings table, which may be representing data other than that which changed.

---
+++ sql/compare_footprint_output.sql
@@ -0,0 +1,13 @@
+Create View Analysis.compare_footprint_output as
+SELECT  p1.parcel_id as gfpid,
+   		p2.parcel_id as runpid,
+		p1.year_built-p2.year_built as diff_year_built,
+		p1.total_job_spaces-p2.job_spaces as diff_job_spaces,
+		p1.total_residential_units-p2.total_residential_units as diff_total_residential_units,
+		p1.Acres-p2.Acres as diff_Acres,
+		p1.People_Per_Acre-p2.People_Per_Acre as diff_People_Per_Acre,
+		p1.Jobs_Per_Acre-p2.Jobs_Per_Acre as diff_Jobs_Per_Acre,
+		p1.Shape
+FROM Analysis.ALT_4_GFP_FC as p1
+LEFT JOIN Analysis.run7224_parcel_output_spatial as p2
+ON p1.parcel_id = p2.parcel_id;
