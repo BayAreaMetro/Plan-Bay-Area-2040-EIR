184c447b1ab7481ff42bf2710a5a6e0171da9ae6
Tom Buckley <buckley.tom@gmail.com>
2017-01-10 20:14:51 -0800

    use diff tables in views
    these are the numbers we expect. the compare output.sql shows almost no differences between the alt_4_fc and the one output with this method

--- sql/Build_Scenario_Footprints.sql
+++ sql/Build_Scenario_Footprints.sql
@@ -1,3 +1,4 @@
+
 --Build All Scenario Growth Footprint Tables using rules defined below:
 --Select parcels where year built is greater than or equal to 2016. 
 --Join to parcel geometry using parcel_id in parcel_shareable_09_01_2016 https://github.com/MetropolitanTransportationCommission/bayarea_urbansim#parcel-geometries
@@ -10,91 +11,101 @@
 Create View Analysis.run10_parcel_output_spatial as
 SELECT  p.COUNTY_ID as county_id, 
 		r.parcel_id as parcel_id, 
-		r.year_built as year_built,
-		r.job_spaces as job_spaces,
-		r.total_residential_units as total_residential_units,
+		cast(r.year_built as float) as year_built,
+		cast(pd.total_job_spaces as float) as total_job_spaces,
+		cast(pd.total_residential_units as float) as total_residential_units,
 		Round(p.shape.STArea()*0.000247105381,2) as Acres,
-		Cast((2.69*r.total_residential_units)/(p.shape.STArea()*0.000247105381) as numeric(18,2)) as People_Per_Acre,
-		Cast((r.job_spaces/(p.shape.STArea()*0.000247105381)) as numeric(18,2)) as Jobs_Per_Acre,
+		Cast((2.69*pd.total_residential_units)/(p.shape.STArea()*0.000247105381) as numeric(18,2)) as People_Per_Acre,
+		Cast((pd.total_job_spaces/(p.shape.STArea()*0.000247105381)) as numeric(18,2)) as Jobs_Per_Acre,
 		p.Shape as Shape
-FROM Analysis.run10_parcel_output as r
+FROM Analysis.run10_parcel_data_diff as pd
+left join Analysis.run10_parcel_output as r
+on pd.parcel_id = r.parcel_id
 LEFT JOIN Analysis.p09_01_2015_parcel_shareable as p
 ON p.parcel_id = r.parcel_id
-WHERE r.year_built>2015;
+WHERE cast(r.year_built AS float)>2015;
 
 GO
 
 CREATE VIEW Analysis.run17_parcel_output_spatial AS
 SELECT  p.COUNTY_ID as county_id, 
 		r.parcel_id as parcel_id, 
-		r.year_built as year_built,
-		r.job_spaces as job_spaces,
-		r.total_residential_units as total_residential_units,
+		cast(r.year_built as integer) as year_built,
+		cast(pd.total_job_spaces as integer) as total_job_spaces,
+		cast(pd.total_residential_units as integer) as total_residential_units,
 		Round(p.shape.STArea()*0.000247105381,2) as Acres,
-		Cast((2.69*r.total_residential_units)/(p.shape.STArea()*0.000247105381) as numeric(18,2)) as People_Per_Acre,
-		Cast((r.job_spaces/(p.shape.STArea()*0.000247105381)) as numeric(18,2)) as Jobs_Per_Acre,
+		Cast((2.69*pd.total_residential_units)/(p.shape.STArea()*0.000247105381) as numeric(18,2)) as People_Per_Acre,
+		Cast((pd.total_job_spaces/(p.shape.STArea()*0.000247105381)) as numeric(18,2)) as Jobs_Per_Acre,
 		p.Shape as Shape
-FROM Analysis.run17_parcel_output as r
+FROM Analysis.run17_parcel_data_diff as pd
+left join Analysis.run17_parcel_output as r
+on pd.parcel_id = r.parcel_id
 left join Analysis.p09_01_2015_parcel_shareable as p
 on p.parcel_id = r.parcel_id
-WHERE r.year_built>2015;
+WHERE cast(r.year_built as integer)>2015;
 
 GO
 
 CREATE VIEW Analysis.run11_parcel_output_spatial AS
 SELECT  p.COUNTY_ID as county_id, 
 		r.parcel_id as parcel_id, 
-		r.year_built as year_built,
-		r.job_spaces as job_spaces,
-		r.total_residential_units as total_residential_units,
+		cast(r.year_built as integer) as year_built,
+		cast(pd.total_job_spaces as integer) as total_job_spaces,
+		cast(pd.total_residential_units as integer) as total_residential_units,
 		Round(p.shape.STArea()*0.000247105381,2) as Acres,
-		Cast((2.69*r.total_residential_units)/(p.shape.STArea()*0.000247105381) as numeric(18,2)) as People_Per_Acre,
-		Cast((r.job_spaces/(p.shape.STArea()*0.000247105381)) as numeric(18,2)) as Jobs_Per_Acre,
+		Cast((2.69*pd.total_residential_units)/(p.shape.STArea()*0.000247105381) as numeric(18,2)) as People_Per_Acre,
+		Cast((pd.total_job_spaces/(p.shape.STArea()*0.000247105381)) as numeric(18,2)) as Jobs_Per_Acre,
 		p.Shape as Shape
-FROM Analysis.run11_parcel_output as r
+FROM Analysis.run11_parcel_data_diff as pd
+left join Analysis.run11_parcel_output as r
+on pd.parcel_id = r.parcel_id
 left join Analysis.p09_01_2015_parcel_shareable as p
 on p.parcel_id = r.parcel_id
-WHERE r.year_built>2015;
+WHERE cast(r.year_built as integer)>2015;
 
 GO 
 
 CREATE VIEW Analysis.run12_parcel_output_spatial AS
 SELECT  p.COUNTY_ID as county_id, 
 		r.parcel_id as parcel_id, 
-		r.year_built as year_built,
-		r.job_spaces as job_spaces,
-		r.total_residential_units as total_residential_units,
+		cast(r.year_built as integer) as year_built,
+		cast(pd.total_job_spaces as integer) as total_job_spaces,
+		cast(pd.total_residential_units as integer) as total_residential_units,
 		Round(p.shape.STArea()*0.000247105381,2) as Acres,
-		Cast((2.69*r.total_residential_units)/(p.shape.STArea()*0.000247105381) as numeric(18,2)) as People_Per_Acre,
-		Cast((r.job_spaces/(p.shape.STArea()*0.000247105381)) as numeric(18,2)) as Jobs_Per_Acre,
+		Cast((2.69*pd.total_residential_units)/(p.shape.STArea()*0.000247105381) as numeric(18,2)) as People_Per_Acre,
+		Cast((pd.total_job_spaces/(p.shape.STArea()*0.000247105381)) as numeric(18,2)) as Jobs_Per_Acre,
 		p.Shape as Shape
-FROM Analysis.run12_parcel_output as r
+FROM Analysis.run12_parcel_data_diff as pd
+left join Analysis.run12_parcel_output as r
+on pd.parcel_id = r.parcel_id
 left join Analysis.p09_01_2015_parcel_shareable as p
 on p.parcel_id = r.parcel_id
-WHERE r.year_built>2015;
+WHERE cast(r.year_built as integer)>2015;
 
 GO 
 
 CREATE VIEW Analysis.run7224_parcel_output_spatial AS
 SELECT  p.COUNTY_ID as county_id, 
 		r.parcel_id as parcel_id, 
-		r.year_built as year_built,
-		r.job_spaces as job_spaces,
-		r.total_residential_units as total_residential_units,
+		cast(r.year_built as integer) as year_built,
+		cast(pd.total_job_spaces as integer) as total_job_spaces,
+		cast(pd.total_residential_units as integer) as total_residential_units,
 		Round(p.shape.STArea()*0.000247105381,2) as Acres,
-		Cast((2.69*r.total_residential_units)/(p.shape.STArea()*0.000247105381) as numeric(18,2)) as People_Per_Acre,
-		Cast((r.job_spaces/(p.shape.STArea()*0.000247105381)) as numeric(18,2)) as Jobs_Per_Acre,
+		Cast((2.69*pd.total_residential_units)/(p.shape.STArea()*0.000247105381) as numeric(18,2)) as People_Per_Acre,
+		Cast((pd.total_job_spaces/(p.shape.STArea()*0.000247105381)) as numeric(18,2)) as Jobs_Per_Acre,
 		p.Shape as Shape
-FROM Analysis.run7224_parcel_output as r
+FROM Analysis.run7224_parcel_data_diff_1 as pd
+left join Analysis.run7224_parcel_output as r
+on pd.parcel_id = r.parcel_id
 left join Analysis.p09_01_2015_parcel_shareable as p
 on p.parcel_id = r.parcel_id
-WHERE r.year_built>2015;
+WHERE cast(r.year_built as integer)>2015;
 
 /*Cast(2.69*alt_4_Diff.total_residential_units as numeric(18,0)) as Estimated_Population, 
 Round(p.shape.STArea()*0.000247105381,2) as Acres,
 Cast((2.69*alt_4_Diff.total_residential_units)/(p.shape.STArea()*0.000247105381) as numeric(18,2)) as People_Per_Acre,
 Cast((alt_4_Diff.total_job_spaces/(p.shape.STArea()*0.000247105381)) as numeric(18,2)) as Jobs_Per_Acre,
 WHERE        (People_Per_Acre >= 8) OR
-                         (Jobs_Per_Acre >= 10)
+                         (Jobs_Per_Acre >= 10)*/
 
 
