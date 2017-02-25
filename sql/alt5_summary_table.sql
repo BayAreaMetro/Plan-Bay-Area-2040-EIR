31cf6f7ff1506b30a4650398c548f5f7b37d576f
Tom Buckley <buckley.tom@gmail.com>
2017-02-24 21:10:30 -0800

    added sql for summary tables
    in order for this sql to output reasonable results, we need to check on the spatial intersection of the underlying tables. see https://mtcdrive.app.box.com/folder/20032459710 for images of the underlying tables.

---
+++ sql/alt5_summary_table.sql
@@ -0,0 +1,33 @@
+create view UrbanSim.Counties_TPAs_Alt_5_Overlay_DataSummary as 
+SELECT 
+COUNTYNAME, 
+Summary_Class, 
+Summary_Class_Description, 
+Shape.STArea()*0.000247105 as Acres,
+ Shape 
+ FROM 
+COUNTIES_TPAS_ALT_5_OVERLAY;
+
+Go 
+--Calc the following 
+--regional total acres 
+select 
+CountyName, 
+Cast(Sum(Acres) as numeric(18,0)) as Total_Acres 
+From UrbanSim.Counties_TPAs_Alt_5_Overlay_DataSummary 
+Group By CountyName 
+Order By CountyName select CountyName, 
+Cast(Sum(Acres) as numeric(18,0)) as Total_Acres_Class_3 
+From UrbanSim.Counties_TPAs_Alt_5_Overlay_DataSummary 
+Where Summary_Class = 'Class_3' 
+Group By CountyName 
+Order By CountyName; 
+
+GO
+
+select CountyName, 
+Cast(Sum(Acres) as numeric(18,0)) as Total_Acres_Class_4 
+From UrbanSim.Counties_TPAs_Alt_5_Overlay_DataSummary 
+Where Summary_Class = 'Class_4' 
+Group By CountyName 
+Order By CountyName; 
